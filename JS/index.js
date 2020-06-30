78488669dd375faebbf39956adc1a28eb135e2cc
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2020-06-30 20:07:13 +0100

    Create index.js

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+  
+/*!
+ * The Stack Project - Additional js code for site functionality
+ * (c) 2020 
+ * Silk Studio - development + design
+ * https://silkstudio.co/
+ */
+
+
+
+
+
+
+// Create link delay for page transitions
+        
+    function delay (URL) {
+        setTimeout( function() { window.location = URL }, 1000 );
+    };
+
+
+// Init quick links to begin listening
+
+    quicklink.listen();
