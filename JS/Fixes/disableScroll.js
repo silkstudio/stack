e42f89720d6b52198a118cab23e339e773472228
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2020-07-06 15:08:55 +0100

    Create disableScroll.js

---
+++ JS/Fixes/disableScroll.js
@@ -0,0 +1,39 @@
+
+var Webflow = Webflow || [];
+let $body = $(document.body);
+let scrollPosition = 0;
+
+Webflow.push(function () {
+    $('[scroll=disable]').on('click', function () {
+        let oldWidth = $body.innerWidth();
+        scrollPosition = window.pageYOffset;
+        $body.css('overflow', 'hidden');
+        $body.css('position', 'fixed');
+        $body.css('top', `-${scrollPosition}px`);
+        $body.width(oldWidth);
+    });
+    $('[scroll=enable]').on('click', function () {
+        if ($body.css('overflow') != 'hidden') { scrollPosition = window.pageYOffset; }
+        $body.css('overflow', '');
+        $body.css('position', '');
+        $body.css('top', '');
+        $body.width('');
+        $(window).scrollTop(scrollPosition);
+    });
+    $('[scroll=both]').on('click', function () {
+        if ($body.css('overflow') != 'hidden') {
+            let oldWidth = $body.innerWidth();
+            scrollPosition = window.pageYOffset;
+            $body.css('overflow', 'hidden');
+            $body.css('position', 'fixed');
+            $body.css('top', `-${scrollPosition}px`);
+            $body.width(oldWidth);
+        } else {
+            $body.css('overflow', '');
+            $body.css('position', '');
+            $body.css('top', '');
+            $body.width('');
+            $(window).scrollTop(scrollPosition);
+        }
+    });
+});
