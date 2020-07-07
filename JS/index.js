e1a2c6cf85fe549b307251a67c28423c4a8403ec
Nathan Hughes <Nathan@Nathans-MacBook-Pro.local>
2020-07-07 20:19:32 +0100

    update scss

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,21 @@
-  
 /*!
  * The Stack Project - Additional js code for site functionality
  * (c) 2020 
  * Silk Studio - development + design
  * https://silkstudio.co/
+ *
+ *
+ * 
+ * 
+ * 
  */
 
-
-
-
-
-
 // Create link delay for page transitions
-        
-    function delay (URL) {
-        setTimeout( function() { window.location = URL }, 1000 );
-    };
-
+function delay(URL) {
+	setTimeout(function() {
+		window.location = URL;
+	}, 1000);
+}
 
 // Init quick links to begin listening
-
-    quicklink.listen();
+quicklink.listen();
