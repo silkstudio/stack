f25de1412cc4f48555e4ed2dddccf51d1dbd3013
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2020-07-12 09:51:12 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,19 +9,36 @@
  * 
  * 
  */
-
-// Create link delay for page transitions
-function delay(URL) {
-	setTimeout(function() {
-		window.location = URL;
-	}, 1000);
-}
-
-// Init quick links to begin listening
-quicklink.listen();
-
+  
 // Remove inline editor styling
-const editorStyles = document.getElementById('editor-styles');
-window.onLoad = () => {
-	editorStyles.style.display = 'none';
-};
+  const editorStyles = document.getElementById('editor-styles');  
+  window.onload = () => {
+	editorStyles.parentNode.removeChild(editorStyles);
+  };
+  
+  
+// Create link delay for page transitions        
+  function delay (URL) {
+    setTimeout( function() { window.location = URL }, 1000 );
+  };
+  
+
+// Set mobile menu size
+  let intViewportWidth = window.innerWidth;
+  let intViewportHeight = window.innerHeight;
+  const mobileNav = document.querySelector('.mobile-nav');
+  mobileNav.style.height = `${intViewportHeight}px`;
+  mobileNav.style.width = `${intViewportWidth}px`;
+
+// Init smooth scroll
+  let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  if (!isMobile && intViewportWidth >= 992) {
+      luxy.init({
+          wrapper: '#luxy',
+          wrapperSpeed: 0.065
+      });
+  } 
+  
+// Init quick links
+  quicklink.listen();
+  
