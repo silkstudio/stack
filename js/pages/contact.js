c65d5c1afcc5d24023749fb6c22a2bca6fdb7e83
Nathan Hughes <Nathan@Nathans-MacBook-Pro.local>
2020-07-13 11:18:39 +0100

    js amends

--- js/pages/contact.js
+++ js/pages/contact.js
@@ -2,16 +2,17 @@
 // Contact.js
 // ==========================================================================
 
+// Change select field styles if active
 const selectField = document.getElementById('Reason-for-contacting');
 
 selectField.addEventListener('change', (event) => {
-  let selectValue = document.getElementById('Reason-for-contacting').value;
+	let selectValue = document.getElementById('Reason-for-contacting').value;
 	if (event.target.value === 'none' && selectField.classList.contains('selected')) {
-  	selectField.classList.remove('selected');
-    selectValue = selectField.value;
-    } else if ( event.target.value !== 'none' && !selectField.classList.contains('selected')){
-      selectField.classList.add('selected');
-      selectValue = selectField.value;
-    } else {
-  }
+		selectField.classList.remove('selected');
+		selectValue = selectField.value;
+	} else if (event.target.value !== 'none' && !selectField.classList.contains('selected')) {
+		selectField.classList.add('selected');
+		selectValue = selectField.value;
+	} else {
+	}
 });
--- js/pages/who-we-are.js
+++ js/pages/who-we-are.js
@@ -2,28 +2,28 @@
 // Who-we-are.js
 // ==========================================================================
 
-// --- Change slider tab on click --- //
+// Change slider tab on click //
 
 // Go to slide 1
 const slideOne = document.querySelector('#youth-focused').addEventListener('click', () => {
-    const bulletOne = document.querySelector("#mission-slider--pseudo-nav div:nth-child(1)");
-    bulletOne.click();
+	const bulletOne = document.querySelector('#mission-slider--pseudo-nav div:nth-child(1)');
+	bulletOne.click();
 });
 
 // Go to slide 2
 const slideTwo = document.querySelector('#collaborative-learning').addEventListener('click', () => {
-    const bulletTwo = document.querySelector("#mission-slider--pseudo-nav div:nth-child(2)");
-    bulletTwo.click();
+	const bulletTwo = document.querySelector('#mission-slider--pseudo-nav div:nth-child(2)');
+	bulletTwo.click();
 });
 
 // Go to slide 3
 const slideThree = document.querySelector('#set-up-for-success').addEventListener('click', () => {
-    const bulletThree = document.querySelector("#mission-slider--pseudo-nav div:nth-child(3)");
-    bulletThree.click();
+	const bulletThree = document.querySelector('#mission-slider--pseudo-nav div:nth-child(3)');
+	bulletThree.click();
 });
 
 // Go to slide 4
 const slideFour = document.querySelector('#just-in-case').addEventListener('click', () => {
-    const bulletFour = document.querySelector("#mission-slider--pseudo-nav div:nth-child(4)");
-    bulletFour.click();
+	const bulletFour = document.querySelector('#mission-slider--pseudo-nav div:nth-child(4)');
+	bulletFour.click();
 });
